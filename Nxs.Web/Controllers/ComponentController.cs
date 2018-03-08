35a197d8d0b82a4fe1892205d727baf575175258
ajianCD <568848142@qq.com>
2018-03-08 15:02:03 +0800

    日报统计页面

---
+++ Nxs.Web/Controllers/ComponentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nxs.Web.Controllers
+{
+    public class ComponentController : Controller
+    {
+        // GET: Component
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 优良中差组件
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Radio_YLZC()
+        {
+            return PartialView();
+        }
+    }
+}
